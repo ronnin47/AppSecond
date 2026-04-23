4e2ee9b5a4e1e62c05686dccc636176b3d9bef1f
ronnin47 <tempesttempest66@gmail.com>
2026-04-23 11:08:14 -0300

    cambio a git

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>segunda app</Text>
+      <Text>cambios para github</Text>
       <StatusBar style="auto" />
     </View>
   );
